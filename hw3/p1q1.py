22329fb958c4f7988c4d4d3eb63dff1a249d4461
Mohamed Khodeir <mohamed@hubdoc.com>
2019-01-29 16:33:50 -0500

    name experiment log file

--- hw3/p1q1.py
+++ hw3/p1q1.py
@@ -10,7 +10,7 @@ def main():
     env = get_env(task, seed)
     session = get_session()
   	
-    atari_learn(env, session, num_timesteps=4 * 5e6, double_q=False)
+    atari_learn(env, session, num_timesteps=4 * 5e6, double_q=False, rew_file='p1q1.pkl')
 
 
 if __name__ == '__main__':
